13617cfb1593c854fa951bc5d32c52312604a743
KazyaVX <kazuyaicikawa@gmail.com>
2024-11-03 22:19:34 +0700

    update

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-const professions = ["Naufal Lutfy N.", "Programmer", "UI/UX"];
+const professions = ["Lutfy"];
         let index = 0;
         let charIndex = 0;
-        let deleting = false;
+        let deleting = true;
 
         function type() {
             const currentText = professions[index];
@@ -10,7 +10,7 @@ const professions = ["Naufal Lutfy N.", "Programmer", "UI/UX"];
             if (!deleting && charIndex <= currentText.length) {
                 // Menulis teks
                 typingContainer.textContent = currentText.slice(0, charIndex++);
-                setTimeout(type, 100);
+                setTimeout(type, 10);
             } else if (deleting && charIndex > 0) {
                 // Menghapus teks
                 typingContainer.textContent = currentText.slice(0, charIndex--);
@@ -28,3 +28,29 @@ const professions = ["Naufal Lutfy N.", "Programmer", "UI/UX"];
         }
 
         document.addEventListener("DOMContentLoaded", type);
+
+        function getGreeting() {
+            const now = new Date();
+            const hours = now.getHours();
+            let greeting;
+
+            if (hours >= 5 && hours < 12) {
+                greeting = "Selamat Pagi!";
+            } else if (hours >= 12 && hours < 17) {
+                greeting = "Selamat Siang!";
+            } else if (hours >= 17 && hours < 21) {
+                greeting = "Selamat Malam!";
+            } else {
+                greeting = "Selamat Malam!";
+            }
+
+            // Untuk menampilkan pesan berdasarkan jam spesifik
+            if (hours === 7) {
+                greeting += "Selamat Pagi Awal!";
+            }
+
+            document.getElementById("greeting").innerText = greeting;
+        }
+
+        // Panggil fungsi untuk menampilkan greeting saat halaman dimuat
+        window.onload = getGreeting;
